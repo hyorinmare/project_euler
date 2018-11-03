8ea889781a9cc777a0fa74df1211ee8109bd9861
hyorinmare <37873400+hyorinmare@users.noreply.github.com>
2018-11-03 04:15:56 +0100

    Add files via upload

--- Problem5.java
+++ Problem5.java
@@ -3,7 +3,20 @@ public class Problem5 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int a = 1;
+		int b = 2;
+		while(b>0) {
+			if((a%1==0) && (a%2 ==0) && (a%3==0) && (a%4 ==0) && (a%5==0) && (a%6 ==0) &&(a%7==0) && (a%8 ==0) && (a%9==0) && (a%10 ==0) && (a%11 ==0) && (a%12 ==0) && (a%13==0) && (a%14 ==0) && (a%15==0) && (a%16 ==0) && (a%17 == 0) && (a%18 == 0) && (a%19 ==0) &&(a%20 == 0)) {
+				System.out.println(a);
+				break;
+				
+			}else {
+				a++;
+			}
+		}
 
+
+		 
 	}
 
 }
