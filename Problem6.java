42166287290856d00100bcdf407db1b5853de7de
hyorinmare <37873400+hyorinmare@users.noreply.github.com>
2018-11-03 04:23:24 +0100

    Add files via upload

---
+++ Problem6.java
@@ -0,0 +1,19 @@
+
+public class Problem6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int sum1 =0;
+		int sum2 = 0;
+		
+		for(int i =1; i<=100; i++) {
+			 sum1 = sum1 +(i*i);
+			 sum2 = sum2 + i;
+		}
+		sum2 = sum2*sum2;
+		System.out.println(sum2-sum1);
+		
+
+	}
+
+}
